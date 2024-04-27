e9e78f5f08efc5e2dcb5600dfb0c42033a0b4819
Guillermo Cardona <memocargo@hotmail.com>
2024-04-27 17:11:28 -0600

    layout added

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/app/lib/Prisma";
+import { Todo } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from 'yup'
 
 interface Segments {
   params: {
@@ -7,15 +9,46 @@ interface Segments {
   }
 }
 
+const getTodo = async (id: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findFirst({ where: { id } })
+  return todo;
+}
+
 export async function GET(request: NextRequest, { params }: Segments) {
-  const elTodo = await prisma.todo.findUnique({
-    where: {
-      id: params.id
-    }
-  });
+  const elTodo = await getTodo(params.id)
 
   if (!elTodo) {
     return NextResponse.json({ error: "Todo not found" }, { status: 404 });
   }
   return NextResponse.json(elTodo);
+}
+
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  completed: yup.boolean().optional(),
+})
+
+
+export async function PUT(request: NextRequest, { params }: Segments) {
+  const { id } = params;
+  const todo = await getTodo(id)
+
+  if (!todo) {
+    return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+  }
+  try {
+
+    const body = await putSchema.validate(await request.json())
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: body
+    })
+
+    return NextResponse.json(updatedTodo);
+  } catch (e) {
+    return NextResponse.json(e, { status: 400 })
+  }
+
 }
---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,7 @@
+export default function RestTodosPage() {
+	return (
+		<div>
+			<h1>Page resttodos</h1>
+		</div>
+	);
+}
